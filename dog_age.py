4e4bd201eb64698b582342629f3f91a99085a70f
timothysailer2 <tlsailer2@outlook.com>
2026-03-16 19:57:27 -0700

    day 1 rev

--- dog_age.py
+++ dog_age.py
@@ -1,4 +1,11 @@
 name = input("Hi! What is your dogs name? ")
-years = int(input(f"How many years have you had {name}?"))
-age = years * 7
-print(f"{name} is {age} in dog years!")
+own = input(f"Have you owned {name} longer than 12 months?")
+if own == "yes":
+    years = int(input("How many years have you owned {name}? "))
+    age = years * 7
+    print(f"{name} is {age} in dog years!")
+else:
+    months = int(input("How many months have you owned {name}? "))
+    age_a = months/12
+    age2 = age_a * 7
+    print(f"{name} is {age2:.1f} in dog years")
