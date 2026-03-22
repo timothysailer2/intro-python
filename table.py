ef90f71ceefe3ff595f1c01de01dced7a17b592f
timothysailer2 <tlsailer2@outlook.com>
2026-03-22 15:53:22 -0700

    day 3

---
+++ table.py
@@ -0,0 +1,7 @@
+print(f"{'Data Structure':<12} {'Syntax':<20} {'Ordered':<8} {'Mutable':<8} {'Duplicates':<18} {'Access Method'}")
+print("-" * 90)
+
+print(f"{'List':<12} {'[1,2,3]':<20} {'Yes':<8} {'Yes':<8} {'Yes':<18} {'Index'}")
+print(f"{'Tuple':<12} {'(1,2,3)':<20} {'Yes':<8} {'No':<8} {'Yes':<18} {'Index'}")
+print(f"{'Dictionary':<12} {'{\"a\":1}':<20} {'Yes':<8} {'Yes':<8} {'No (keys)':<18} {'Key'}")
+print(f"{'Set':<12} {'{1,2,3}':<20} {'No':<8} {'Yes':<8} {'No':<18} {'No index'}")
